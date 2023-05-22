81d8da176bebb8ca4e1274a2353f1c67addf409e
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-05-22 09:40:29 +0300

    commit tree 80baf4a7b4a6d4090fb829b5d7a6c81b4da4f189

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,23 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre, :current_page
+
+    def initialize(title)
+        @title = title
+        @current_page = 1
+    end
+    def turn_page
+        @current_page += 1
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+book = Book.new("And Then There Were None")
+book.author = "Agatha Christie"
+book.page_count = 272
+book.genre="Mystery"
+book.turn_page
 
 
+puts book.title
+puts book.author
+puts book.page_count
+puts book.title
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,49 @@
-# Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size , :material, :condition, :repaired
+    
+    def initialize(brand)
+        @brand = brand
+        @condition = "new"
+        @repaired = false
+    end
+    def shoe_properties
+        puts "Brand: #{@brand}"
+        puts "Condition: #{@condition}"
+        puts "Repaired: #{@repaired ? 'Yes' : 'No'}"
+    end
+    def cobble
+        @repaired = true
+        puts = "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
+
+shoe1 = Shoe.new("Adidas")
+shoe1.shoe_properties
+shoe1.color = "red"
+shoe1.size = 9.5
+shoe1.material = "suede"
+shoe1.condition="tattered"
+shoe1.cobble
+
+shoe2 = Shoe.new("Nike")
+shoe2.shoe_properties
+shoe2.color = "red"
+shoe2.size = 9.5
+shoe2.material = "suede"
+shoe2.condition = "tattered"
+
+
+shoe2.cobble
+shoe2.shoe_properties
+
+puts shoe1.brand
+puts shoe2.brand
+puts shoe1.color
+puts shoe2.color
+puts shoe1.size
+puts shoe2.size
+puts shoe1.material
+puts shoe2.material
+puts shoe1.condition
+puts shoe2.condition
